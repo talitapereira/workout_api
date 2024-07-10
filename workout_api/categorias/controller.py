47cab9e413fc952a63845e5a26b941faf92bc906
talitapereira <talitarsp@outlook.com>
2024-07-10 16:30:24 -0300

    Desenvolvimento do desafio

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -1,14 +1,13 @@
 from uuid import uuid4
 from fastapi import APIRouter, Body, HTTPException, status
 from pydantic import UUID4
+from sqlalchemy.exc import IntegrityError
 from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
 from workout_api.categorias.models import CategoriaModel
 
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
-
 router = APIRouter()
-
 @router.post(
     '/', 
     summary='Criar uma nova Categoria',
@@ -19,15 +18,26 @@ async def post(
     db_session: DatabaseDependency, 
     categoria_in: CategoriaIn = Body(...)
 ) -> CategoriaOut:
-    categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
-    categoria_model = CategoriaModel(**categoria_out.model_dump())
-    
-    db_session.add(categoria_model)
-    await db_session.commit()
+    try:
+        categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
+        categoria_model = CategoriaModel(**categoria_out.model_dump())
+
+        db_session.add(categoria_model)
+        await db_session.commit()
+    except IntegrityError:
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            detail=f"Já existe uma categoria cadastrada com o nome: {categoria_model.nome}",
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Ocorreu um erro ao inserir os dados no banco",
+        )
 
     return categoria_out
-    
-    
+
+       
 @router.get(
     '/', 
     summary='Consultar todas as Categorias',
@@ -38,8 +48,6 @@ async def query(db_session: DatabaseDependency) -> list[CategoriaOut]:
     categorias: list[CategoriaOut] = (await db_session.execute(select(CategoriaModel))).scalars().all()
     
     return categorias
-
-
 @router.get(
     '/{id}', 
     summary='Consulta uma Categoria pelo id',
@@ -50,7 +58,6 @@ async def get(id: UUID4, db_session: DatabaseDependency) -> CategoriaOut:
     categoria: CategoriaOut = (
         await db_session.execute(select(CategoriaModel).filter_by(id=id))
     ).scalars().first()
-
     if not categoria:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
--- workout_api/centro_treinamento/controller.py
+++ workout_api/centro_treinamento/controller.py
@@ -1,14 +1,13 @@
 from uuid import uuid4
 from fastapi import APIRouter, Body, HTTPException, status
 from pydantic import UUID4
+from sqlalchemy.exc import IntegrityError
 from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
 
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
-
 router = APIRouter()
-
 @router.post(
     '/', 
     summary='Criar um novo Centro de treinamento',
@@ -19,15 +18,29 @@ async def post(
     db_session: DatabaseDependency, 
     centro_treinamento_in: CentroTreinamentoIn = Body(...)
 ) -> CentroTreinamentoOut:
-    centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
-    centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
-    
-    db_session.add(centro_treinamento_model)
-    await db_session.commit()
+        try:
+            centro_treinamento_out = CentroTreinamentoOut(
+                id=uuid4(), **centro_treinamento_in.model_dump()
+            )
+            centro_treinamento_model = CentroTreinamentoModel(
+                **centro_treinamento_out.model_dump()
+            )
 
-    return centro_treinamento_out
-    
-    
+            db_session.add(centro_treinamento_model)
+            await db_session.commit()
+        except IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_303_SEE_OTHER,
+                detail=f"Já existe um centro de treinamento cadastrado com o nome: {centro_treinamento_model.nome}",
+            )
+        except Exception:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Ocorreu um erro ao inserir os dados no banco",
+            )
+
+        return centro_treinamento_out
+
 @router.get(
     '/', 
     summary='Consultar todos os centros de treinamento',
@@ -40,8 +53,6 @@ async def query(db_session: DatabaseDependency) -> list[CentroTreinamentoOut]:
     ).scalars().all()
     
     return centros_treinamento_out
-
-
 @router.get(
     '/{id}', 
     summary='Consulta um centro de treinamento pelo id',
@@ -52,7 +63,6 @@ async def get(id: UUID4, db_session: DatabaseDependency) -> CentroTreinamentoOut
     centro_treinamento_out: CentroTreinamentoOut = (
         await db_session.execute(select(CentroTreinamentoModel).filter_by(id=id))
     ).scalars().first()
-
     if not centro_treinamento_out:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
